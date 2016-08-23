87b93c50336a3b5e2ecb4495315295281f29c7da
tom trinneer <tom@trinneer.com>
2016-08-23 13:29:47 -0700

    added intents

--- app.js/server.js
+++ app.js/server.js
@@ -20,6 +20,29 @@ var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
 // bot dialogs
-bot.dialog('/', function (session) {
-    session.send("Hello World");
+bot.dialog('/', intents);
+
+intents.matches(/^change name/i [
+    function (session) {
+        session.beginDialog('/profile');
+    },
+    function (session, results) {
+        session.send('OK... changed your gem to %s', session.userData.name);
+    }
 });
+
+intents.onDefault([
+    function (session, results) {
+        session.send('Hello %s!', sessoin.userData.name);
+    }
+]);
+
+bot.dialog('/profile'. [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! Which gem are you?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
