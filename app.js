010e45124ad093073d6bd6ee9edaac6262c9533a
trinneert <tom@trinneer.com>
2016-10-14 15:41:24 -0700

    receipt still

--- app.js
+++ app.js
@@ -122,20 +122,23 @@ bot.dialog('/help', [
 
 bot.dialog('/donate', [
     function(session) {
-        var msgDonate = new builder.ReceiptCard(session)
-            .title('Help fund this bot')
-            .facts([
-                builder.facts.create(session, 'PayPal', 'Payment method'),
-            ])
-            .items([
-                builder.ReceiptItem.create(session, '$1.00', 'Donation'),
-            ])
-            .tax('$0.00')
-            .total('$1.00')
-            .buttons([
-                builder.CardAction.openUrl(session, 'https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=4Z9M75GEL9CAY', 'Donate')
-            ]);
-    session.endDialog(msgDonate);
+        var msgDonate = new builder.Message(session)
+        .attachments([
+            new builder.ReceiptCard(session)
+                .title('Help fund this bot')
+                .facts([
+                    builder.facts.create(session, 'PayPal', 'Payment method'),
+                ])
+                .items([
+                    builder.ReceiptItem.create(session, '$1.00', 'Donation'),
+                ])
+                .tax('$0.00')
+                .total('$1.00')
+                .buttons([
+                    builder.CardAction.openUrl(session, 'https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=4Z9M75GEL9CAY', 'Donate')
+                ])
+        ]);        
+        session.endDialog(msgDonate);
     }        
 ]);
 
